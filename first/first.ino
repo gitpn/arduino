419175e73776345b8f8e86d185bb7bfb97ec7942
rex <panter2004@gmail.com>
2016-12-01 12:51:30 +0300

    12

--- first/first.ino
+++ first/first.ino
@@ -1,23 +1,3 @@
-#include <ArduinoRobotMotorBoard.h>
-#include <EasyTransfer2.h>
-#include <LineFollow.h>
-#include <Multiplexer.h>
-
-#include <ArduinoRobot.h>
-#include <Arduino_LCD.h>
-#include <Compass.h>
-#include <EasyTransfer2.h>
-#include <EEPROM_I2C.h>
-#include <Fat16.h>
-#include <Fat16Config.h>
-#include <Fat16mainpage.h>
-#include <Fat16util.h>
-#include <FatStructs.h>
-#include <Multiplexer.h>
-#include <SdCard.h>
-#include <SdInfo.h>
-#include <Squawk.h>
-#include <SquawkSD.h>
 
 void setup() {
   // put your setup code here, to run once:
