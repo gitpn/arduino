01e74dc37e6a5fb3617d306bcbccf614d54ea731
rex <panter2004@gmail.com>
2016-12-02 08:36:13 +0300

    12

--- first/first.ino
+++ first/first.ino
@@ -1,19 +1,30 @@
+//инициализация пинов
 int ledPin = 13; //светодиод
-int switchPin = 7;
+int switchPin = 7; //7 пин на плате
+
 void setup()
 {
- pinMode (ledPin, OUTPUT);
- pinMode (switchPin, INPUT_PULLUP);
+ pinMode (ledPin, OUTPUT);// светодиод как выход
+ pinMode (switchPin, INPUT_PULLUP);// 7 пин как вход
+ Serial.begin(9600); // установка скорости порта
 }
 
 void loop()
 {
+  //если напряжения на 7 пине нет, то светодиод моргает быстро. провод замкнут
 if(digitalRead(switchPin)==LOW) {
-    flashLed(1000);
-}
-else{flashLed(100);}
+    flashLed(100);
+    Serial.println("Connected");
 }
 
+// если провод разомкнут, на 7 пине есть напряжение, то светодиод моргает медленно
+else{flashLed(1000);
+Serial.println("Not connected");
+}
+
+}
+
+//функция моргания светодиода
 void flashLed(int delayPeriod)
 {
 digitalWrite(ledPin, HIGH);
